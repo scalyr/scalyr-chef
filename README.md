f89a1e05add99ffd7443dd02ac42d520ed23a90e
Imron Alston <imron@imralsoftware.com>
2017-05-16 14:55:52 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# scalyr_agent
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: scalyr_agent
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
