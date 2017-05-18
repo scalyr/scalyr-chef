db4b702354807bd0b6bbac48efd8844f57603a06
Imron Alston <imron@imralsoftware.com>
2017-05-18 19:50:16 +0000

    rpm and variables

--- recipes/update_server_attributes.rb
+++ recipes/update_server_attributes.rb
@@ -1,4 +1,17 @@
 
+if node.default['scalyr_agent']['server_attributes'].nil? then
+  if node['scalyr_agent']['hostname'] then
+    node.default['scalyr_agent']['server_attributes'] = { "serverHost" => node['scalyr_agent']['hostname'] }
+  end
+else
+  if node['scalyr_agent']['hostname'] then
+    node.default['scalyr_agent']['server_attributes']['serverHost'] = node['scalyr_agent']['hostname']
+  end
+end
+
 template '/etc/scalyr-agent-2/agent.d/server_attributes.json' do
   source 'server_attributes.json.erb'
+  variables( {
+    :server_attributes => node['scalyr_agent']['server_attributes']
+  })
 end
