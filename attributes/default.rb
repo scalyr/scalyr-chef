e3e11d40e411c78b875b540f17af254ef8920276
Dave Steinberg <dave@steinbergcomputing.com>
2017-06-06 14:48:39 +0000

    support open-ended agent configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
-
 default['scalyr_agent']['api_key'] = nil
 
 default['scalyr_agent']['scalyr_server'] = nil
@@ -6,5 +5,6 @@ default['scalyr_agent']['scalyr_server'] = nil
 default['scalyr_agent']['hostname'] = nil
 
 default['scalyr_agent']['logs'] = {}
+default['scalyr_agent']['agent_settings'] = {}
 default['scalyr_agent']['server_attributes'] = {}
 default['scalyr_agent']['monitors'] = {}
